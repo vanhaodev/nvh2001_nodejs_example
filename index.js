4a25554def22544f29065e3fff209cd083a95f84
Nguyen Van Hao <86046654+vanhaodev@users.noreply.github.com>
2021-12-02 10:43:00 +0700

    nvh2001

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 2001
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,4 +8,5 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
   .get('/p2', (req, res) => res.render('index2'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, '127.0.0.1')
+  console.log('Port là 2001 nhá');
